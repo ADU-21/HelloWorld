25eb2f8f6a3bebed36c5b87572f1b6c1e4de7572
Tan Yixiu <yixiu.tan@gmail.com>
2016-07-13 11:03:46 +0800

    hi

--- src/main/java/com/tw/helloworld/GreetingController.java
+++ src/main/java/com/tw/helloworld/GreetingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String TEMPLATE = "Hey, %s!";
+    private static final String TEMPLATE = "Hey ~ %s!";
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name",defaultValue = "world") String name){
